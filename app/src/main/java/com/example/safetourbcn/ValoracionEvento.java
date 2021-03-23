812b79537a9171227eef9d023940f153995f7e16
victorcolladopreciado <victor.collado.preciado@estudiantat.upc.edu>
2021-03-24 00:11:02 +0100

    Commit ValoracionEvento

--- app/src/main/java/com/example/safetourbcn/ValoracionEvento.java
+++ app/src/main/java/com/example/safetourbcn/ValoracionEvento.java
@@ -1,4 +1,60 @@
 package com.example.safetourbcn;
 
 public class ValoracionEvento extends Valoracion {
+    private String idEvento;
+
+    public ValoracionEvento(int puntuacion, String comentario, String idValoracion, Boolean conReserva, Boolean[] medidas, String idusuario, String idevento) {
+        super(puntuacion, comentario, idValoracion, conReserva, medidas, idusuario);
+        idEvento = idevento;
+    }
+
+    @Override
+    public String getIdUsuario() {
+        return super.getIdUsuario();
+    }
+
+    @Override
+    public Boolean getConReserva() {
+        return super.getConReserva();
+    }
+
+    @Override
+    public Boolean[] getMedidas() {
+        return super.getMedidas();
+    }
+
+    @Override
+    public int getPuntuacion() {
+        return super.getPuntuacion();
+    }
+
+    @Override
+    public String getComentario() {
+        return super.getComentario();
+    }
+
+    @Override
+    public String getIdValoracion() {
+        return super.getIdValoracion();
+    }
+
+    public String getIdEvento() {
+        return idEvento;
+    }
+
+    @Override
+    public void setComentario(String comentario) {
+        super.setComentario(comentario);
+    }
+
+    @Override
+    public void setMedidas(Boolean[] medidas) {
+        super.setMedidas(medidas);
+    }
+
+    @Override
+    public void setPuntuacion(int puntuacion) {
+        super.setPuntuacion(puntuacion);
+    }
+
 }
