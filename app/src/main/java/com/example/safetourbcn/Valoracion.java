1132deb179ea769929f3db591a6247270d2a0b02
victorcolladopreciado <victor.collado.preciado@estudiantat.upc.edu>
2021-03-24 00:10:07 +0100

    Commit Valoracion

--- app/src/main/java/com/example/safetourbcn/Valoracion.java
+++ app/src/main/java/com/example/safetourbcn/Valoracion.java
@@ -1,4 +1,57 @@
 package com.example.safetourbcn;
 
 public class Valoracion {
+    private int puntuacion;
+    private String comentario;
+    private String idValoracion;
+    private Boolean ConReserva;
+    private Boolean[] medidas = new Boolean[4];
+    private String idUsuario;
+
+
+    public Valoracion(){}
+    public Valoracion(int puntuacion, String comentario, String idValoracion, Boolean conReserva, Boolean[] medidas, String idusuario) {
+        this.puntuacion = puntuacion;
+        this.comentario = comentario;
+        this.idValoracion = idValoracion;
+        ConReserva = conReserva;
+        this.medidas = medidas;
+        idUsuario = idusuario;
+    }
+
+    public int getPuntuacion() {
+        return puntuacion;
+    }
+
+    public String getIdValoracion() {
+        return idValoracion;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public Boolean getConReserva() {
+        return ConReserva;
+    }
+
+    public Boolean[] getMedidas() {
+        return medidas;
+    }
+
+    public String getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+    public void setMedidas(Boolean[] medidas) {
+        this.medidas = medidas;
+    }
+
+    public void setPuntuacion(int puntuacion) {
+        this.puntuacion = puntuacion;
+    }
 }
