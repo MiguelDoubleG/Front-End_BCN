35c5c00c2189a55b28f47f505646e6495887dbbf
victorcolladopreciado <victor.collado.preciado@estudiantat.upc.edu>
2021-03-24 00:05:07 +0100

    Commit Evento

--- app/src/main/java/com/example/safetourbcn/Evento.java
+++ app/src/main/java/com/example/safetourbcn/Evento.java
@@ -1,4 +1,93 @@
 package com.example.safetourbcn;
 
+import java.util.Date;
+
 public class Evento {
+    private String localizacion;
+    private String descripcion;
+    private String idEvento;
+    private int PlazasDisponibles;
+    private Date date;
+    private String horaInici;
+    private String horaFinal;
+    private String idEmpresa;
+
+    public Evento(){
+    }
+    public Evento(String loc, String desc, String id, int plazas, Date fecha, String inicio, String horafinal, String empresa){
+        localizacion = loc;
+        descripcion = desc;
+        idEvento = id;
+        PlazasDisponibles = plazas;
+        horaInici = inicio;
+        horaFinal = horafinal;
+        idEmpresa = empresa;
+    }
+
+    public String getLocalizacion() {
+        return localizacion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public String getHoraFinal() {
+        return horaFinal;
+    }
+
+    public String getHoraInici() {
+        return horaInici;
+    }
+
+    public int getPlazasDisponibles() {
+        return PlazasDisponibles;
+    }
+
+    public String getIdEvento() {
+        return idEvento;
+    }
+
+    public String getIdEmpresa() {
+        return idEmpresa;
+    }
+
+    public void setLocalizacion(String localizacion) {
+        this.localizacion = localizacion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public void setHoraFinal(String horaFinal) {
+        this.horaFinal = horaFinal;
+    }
+
+    public void setHoraInici(String horaInici) {
+        this.horaInici = horaInici;
+    }
+
+    public void setIdEvento(String idEvento) {
+        this.idEvento = idEvento;
+    }
+
+    public void setPlazasDisponibles(int plazasDisponibles) {
+        PlazasDisponibles = plazasDisponibles;
+    }
+
+    /*
+    public void setIdEmpresa(String idEmpresa) {
+        this.idEmpresa = idEmpresa;
+    }
+
+     */
 }
