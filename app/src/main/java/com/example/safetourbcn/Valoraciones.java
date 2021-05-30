068991506881ee494c9043d9c97a8e8013a87b8a
victorcolladopreciado <victor.collado.preciado@estudiantat.upc.edu>
2021-05-30 18:43:36 +0200

    Infowindow

--- app/src/main/java/com/example/safetourbcn/Valoraciones.java
+++ app/src/main/java/com/example/safetourbcn/Valoraciones.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 public class Valoraciones extends AppCompatActivity {
 
     @Override
@@ -12,7 +14,29 @@ public class Valoraciones extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_valoraciones);
 
+
+
         final TextView name_establish = findViewById(R.id.nombre_establecimiento);
         name_establish.setText(getIntent().getExtras().getString("establishment"));
+
+        PlacesList pl = PlacesList.getInstance();
+        int b = 0;
+        String direccion = null;
+        String horario = null;
+        float rating = 0;
+        for (int i = 0; i < pl.getLength() && b == 0; ++i) {
+            Establishment place = pl.getEstablishment(i);
+            if(place.getName().equals(name_establish.getText())){
+                b = 1;
+                direccion = place.getAddress();
+                horario = place.getSchedule();
+                rating = place.getRating();
+            }
+
+        }
+        final TextView dir_establish = findViewById(R.id.direccion_establecimiento);
+        dir_establish.setText(direccion);
+        final TextView horari = findViewById(R.id.horario);
+        horari.setText(horario);
     }
 }
