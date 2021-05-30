37d835d1955508e602ab53c547d8076a463be869
Quetz <miguel.alejandro.gil@est.fib.upc.edu>
2021-05-30 16:52:58 +0200

    works but stupid emulator wont run

--- app/src/main/java/com/example/safetourbcn/EditProfileActivity.java
+++ app/src/main/java/com/example/safetourbcn/EditProfileActivity.java
@@ -1,45 +1,35 @@
 package com.example.safetourbcn;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
-import androidx.fragment.app.FragmentActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
-/*
- * Main Activity class that loads {@link MainFragment}.
- */
-public class EditProfileActivity extends FragmentActivity {
-
+public class EditProfileActivity extends AppCompatActivity {
+    Session session = Session.getInstance();
+    //BackEndRequests ber = BackEndRequests.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Session session = Session.getInstance();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
-
-        // Get the Intent that started this activity and extract the string
-        Intent intent = getIntent();
-
-        // Capture the layout's TextView and set the string as its text
-        //TextView textView = findViewById(R.id.textView);
-        //textView.setText(message);
+        //final TextView correoTextView = findViewById(R.id.correo);
         final TextView correoTextView = findViewById(R.id.correo);
         final TextView nombreTextView = findViewById(R.id.nombre);
-        /* correo tiene que ser el correo del usuario actual*/
+        /* email must be the user of the current user*/
 
         String correo = session.getEmail();
         int N = correo.length();
         correoTextView.setText(correo.toCharArray(), 0, N-1);
-        /* usuario tiene que ser el nombre del usuario actual*/
+        /* user must be the name of the current user*/
 
         String usuario = session.getName();
         int M = usuario.length();
         nombreTextView.setText(usuario.toCharArray(), 0, M-1);
     }
-
-    public void confirm(View view) {
-        Intent intent = new Intent(this, EditProfileActivity.class);
-        startActivity(intent);
+    public void save(View view) {
+        final TextView nombreTextView = findViewById(R.id.nombre);
+        String newUsername = nombreTextView.getText().toString();
+        session.editUserName(newUsername);
     }
 }
