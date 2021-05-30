80fe5fdcb2780a421ea8f12561fe3a57cd696965
Quetz <miguel.alejandro.gil@est.fib.upc.edu>
2021-05-30 02:43:48 +0200

    sdl

--- app/src/main/java/com/example/safetourbcn/EditProfileActivity.java
+++ app/src/main/java/com/example/safetourbcn/EditProfileActivity.java
@@ -1,20 +1,29 @@
 package com.example.safetourbcn;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
-public class PerfilUsuarioActivity extends AppCompatActivity {
-    Session session = Session.getInstance();
-    BackEndRequests ber = BackEndRequests.getInstance();
+import androidx.fragment.app.FragmentActivity;
+
+/*
+ * Main Activity class that loads {@link MainFragment}.
+ */
+public class EditProfileActivity extends FragmentActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Session session = Session.getInstance();
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_perfil_usuario);
+        setContentView(R.layout.activity_edit_profile);
 
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+
+        // Capture the layout's TextView and set the string as its text
+        //TextView textView = findViewById(R.id.textView);
+        //textView.setText(message);
         final TextView correoTextView = findViewById(R.id.correo);
         final TextView nombreTextView = findViewById(R.id.nombre);
         /* correo tiene que ser el correo del usuario actual*/
@@ -28,7 +37,8 @@ public class PerfilUsuarioActivity extends AppCompatActivity {
         int M = usuario.length();
         nombreTextView.setText(usuario.toCharArray(), 0, M-1);
     }
-    public void editProfile(View view) {
+
+    public void confirm(View view) {
         Intent intent = new Intent(this, EditProfileActivity.class);
         startActivity(intent);
     }
