b82a91b54c01e21c7c683d681557fe39787af83b
victorcolladopreciado <victor.collado.preciado@estudiantat.upc.edu>
2021-06-02 14:49:01 +0200

    establishment actualizado2

---
+++ app/src/main/java/com/example/safetourbcn/Valoraciones.java
@@ -0,0 +1,53 @@
+package com.example.safetourbcn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class Valoraciones extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_valoraciones);
+
+
+
+        final TextView name_establish = findViewById(R.id.nombre_establecimiento);
+        name_establish.setText(getIntent().getExtras().getString("establishment"));
+
+        PlacesList pl = PlacesList.getInstance();
+        int b = 0;
+        String direccion = null;
+        Integer horaapertura = null, horacierre = null;
+        for (int i = 0; i < pl.getLength() && b == 0; ++i) {
+            Establishment place = pl.getEstablishment(i);
+            if(place.getName().equals(name_establish.getText())){
+                b = 1;
+                direccion = place.getAddress();
+                horaapertura = place.getHouropen();
+                horacierre = place.getHourclose();
+            }
+
+        }
+        final TextView dir_establish = findViewById(R.id.direccion_establecimiento);
+        dir_establish.setText(direccion);
+
+    }
+    public void goToInsta (View view) {
+        String url = "http://www.instagram.com";
+        Uri uriUrl = Uri.parse(url);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(launchBrowser);
+    }
+    public void goToUrl (View view) {
+        String url = "http://www.google.com";
+        Uri uriUrl = Uri.parse(url);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(launchBrowser);
+    }
+}
