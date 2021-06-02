7245d9843f0a0a97130e2a98026b9dc418d3bb7f
victorcolladopreciado <victor.collado.preciado@estudiantat.upc.edu>
2021-06-02 13:42:39 +0200

    Comit

--- app/src/main/java/com/example/safetourbcn/Valoraciones.java
+++ app/src/main/java/com/example/safetourbcn/Valoraciones.java
@@ -1,11 +1,24 @@
 package com.example.safetourbcn;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.model.Marker;
+
 import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 
 public class Valoraciones extends AppCompatActivity {
 
@@ -36,7 +49,50 @@ public class Valoraciones extends AppCompatActivity {
         }
         final TextView dir_establish = findViewById(R.id.direccion_establecimiento);
         dir_establish.setText(direccion);
-        final TextView horari = findViewById(R.id.horario);
-        horari.setText(horario);
+        final Spinner spinner = findViewById(R.id.spinner);
+        int d = 4;
+        ArrayList<String> arrayList = new ArrayList<>();
+        int horainici= 8;
+        int horafinal = 20;
+        for(int i = horainici; i<= horafinal; i++){
+            arrayList.add(Integer.toString(i)+"h");
+        }
+        /*arrayList.add(Integer.toString(d)+"h");
+        arrayList.add("9h");
+        arrayList.add("10h");
+        arrayList.add("11h");
+        arrayList.add("12h");
+         */
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, arrayList);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(arrayAdapter);
+
+        final CalendarView calendario= findViewById(R.id.calendar);
+        final EditText number = findViewById(R.id.number_personas);
+        final Button button = findViewById(R.id.boton_reservar);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String texto = spinner.getSelectedItem().toString();
+                if(number.getText().toString().equals("")) {
+                    specify_number();
+                }
+                else{
+
+                }
+            }
+        });
+
     }
+    public void hey() {
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
+    }
+
+    public void specify_number(){
+        AlertDialog alertDialog = new AlertDialog.Builder(this).setTitle("You have to specify a number of people").setNeutralButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {}
+        }).show();
+    }
+
 }
